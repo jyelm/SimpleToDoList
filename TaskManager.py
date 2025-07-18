11126ce3a217734ef53339e90cc065b0d9d2fd7e
jyelm <jyelmo06@gmail.com>
2025-07-18 16:11:06 -0500

    changes using argparse and functionality to the code

---
+++ TaskManager.py
@@ -0,0 +1,85 @@
+"""
+The task manager needs to be able to store items in memory through descriptive words such as add, 
+remove, find, list, and clear. The user should be able to retrieve the from the to do list by keyword,
+one issue that may arise is that when the user inputs different entries, which will be differed by their id, 
+with keywords both in their descirption, the to do list should be able to display both items. Maybe add a feature
+where you can choose to group items, this may just add complexity when it comes to adding functinoality to the input of certain
+descriptors
+"""
+from queue import Queue as q
+import argparse
+
+class TaskManager:
+    """
+    for the scope of this porject, we can keep tasks as a class attribute, so when changed stays the same on every instance the task manager 
+    class is created an object
+    """
+    ids = 1
+    tasks = {}
+    @classmethod
+    def execute(cls, keyword:str, phrase=False):
+        action = getattr(cls, keyword)
+        try:
+            if phrase:
+                return action(phrase)
+            else:
+                return action()
+        except:
+            raise AttributeError(f"No action with {keyword}")
+    @classmethod
+    def add(cls, phrase):
+        cls.tasks[cls.ids] = phrase
+        cls.ids += 1
+    @classmethod
+    def remove(cls, phrase, id):
+        cls.tasks = {key:value for key, value in cls.tasks.items() if key != id} 
+    @classmethod
+    def show(cls):
+        for i in cls.tasks.values():
+            print(f"{i}\n")
+    @classmethod
+    def search(cls, keyword):
+        pass
+
+TaskManager.execute("add", "milk")
+TaskManager.execute("show")
+
+
+"""
+def main():
+    def int_or_str(value):
+        try:
+            return int(value)
+        except ValueError:
+            return value
+
+    parser = argparse.ArgumentParser(
+        description="A simple to-do list storer and accessor"
+    )
+    parser.add_argument(
+        "function",
+        type = str,
+        help = "what do you want to do with the todo list?"
+    )
+    parser.add_argument(
+        "item",
+        type = int_or_str,
+        nargs = "?", #makes the argument optional
+        help = "the id of the entry you want to access or the phrase"
+    )
+    args = parser.parse_args()
+
+if __name__ == "__main__":
+    main()
+"""
+
+
+
+
+
+
+
+
+
+         
+
